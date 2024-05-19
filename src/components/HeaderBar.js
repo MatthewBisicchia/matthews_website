a5648c38d62d6b7a5a0a9ee6d93c421b70f4912c
Matthew Bisicchia <matthewbisicchia@gmail.com>
2024-05-18 20:27:30 -0400

    Fix routes

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -5,7 +5,7 @@ const HeaderBar = () => {
     return(
         <>
             <header id={styles.header}>
-                <div style={{color:'gold'}}><a href="https://www.matthew-bisicchia.com/" id={styles.siteName}>matthew-bisicchia.com</a></div>
+                <div style={{color:'gold'}}><a href="https://matthewbisicchia.github.io/matthews_website/#home" id={styles.siteName}>Matthew Bisicchia</a></div>
             </header>
         </>
     );
