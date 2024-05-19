11650d52d742b27f4a6a720aa0283dce0f20531a
Matthew Bisicchia <matthewbisicchia@gmail.com>
2024-05-18 20:04:30 -0400

    Fix routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import HeaderBar from './components/HeaderBar.js';
 import { useRoutes } from 'react-router-dom';
 import routes from './routes.js';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './components/HomePage.js';
+import AboutPage from './components/AboutPage.js';
+import CookingPage from './components/CookingPage.js';
+
 
 const App = () => {
-
-    const elementsToRoute = useRoutes(routes);
 
     return(
         <>
             <HeaderBar />
-            {elementsToRoute}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/matthews_website/home" element={<HomePage />} />
+                    <Route path="/matthews_website/about" element={<AboutPage />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
